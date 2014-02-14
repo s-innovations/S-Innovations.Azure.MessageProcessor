df5282e6c17b19608c677fbbde476a61768a0c17
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-02-14 21:10:45 +0100

    Moving optiosn to its own file

---
+++ AzureWebrole.MessageProcessor.ServiceBus/ServiceBusMessageProcessorProviderOptions.cs
@@ -0,0 +1,22 @@
+﻿using AzureWebrole.MessageProcessor.Core;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureWebRole.MessageProcessor.ServiceBus
+{
+    public class ServiceBusMessageProcessorProviderOptions : IMessageProcessorProviderOptions<BrokeredMessage>
+    {
+        public string ConnectionString { get; set; }
+        public TopicDescription TopicDescription { get; set; }
+        public SubscriptionDescription SubscriptionDescription { get; set; }
+        public QueueDescription QueueDescription { get; set; }
+
+        public int MaxConcurrentProcesses { get; set; }
+
+        public int MaxMessageRetries { get; set; }
+    }
+}
