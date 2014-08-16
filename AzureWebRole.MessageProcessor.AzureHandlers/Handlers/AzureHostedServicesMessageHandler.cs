0e8235aa00d0af25029bccaa2a0f6c88a3a11f58
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-08-16 19:17:04 +0200

    Missing FIles

---
+++ AzureWebRole.MessageProcessor.AzureHandlers/Handlers/AzureHostedServicesMessageHandler.cs
@@ -0,0 +1,155 @@
+﻿using AzureWebRole.MessageProcessor.AzureHandlers.Messages;
+using AzureWebRole.MessageProcessor.Core;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AzureWebRole.MessageProcessor.AzureHandlers.Handlers
+{
+    public class AzureHostedServicesMessageHandler : IMessageHandler<DeployAzureHostedServiceMessage>
+    {
+        private readonly ICertificateLocator certificates;
+        public AzureHostedServicesMessageHandler(ICertificateLocator certificates)
+        {
+            this.certificates = certificates;
+        }
+        public async Task HandleAsync(DeployAzureHostedServiceMessage message)
+        {
+          
+            SubscriptionCloudCredentials cred = 
+                !string.IsNullOrWhiteSpace(message.AzureSubscriptionCertificateThumbprint) ? 
+                  new CertificateCloudCredentials(message.AzureSubscriptionId,
+                    await certificates.GetCertificateAsync(message.AzureSubscriptionCertificateThumbprint)) as SubscriptionCloudCredentials :
+                (!string.IsNullOrWhiteSpace(message.AzureSubscriptionToken) ? 
+                    new TokenCloudCredentials(message.AzureSubscriptionId) : null);
+
+            if (cred == null)
+                throw new Exception("No Credentials Given");
+
+            using (var management = CloudContext.Clients.CreateComputeManagementClient(cred))
+            {
+                var services = await management.HostedServices.ListAsync();
+
+                if (!services.Any(s => s.ServiceName.Equals(message.HostedServiceName)))
+                {
+                    await CreateHostedService(message, management);
+                }
+
+
+                var service = await management.HostedServices.GetDetailedAsync(message.HostedServiceName);
+
+                if (service.Deployments.Any() && !message.CanUpgrade)
+                {
+                    Trace.TraceInformation("The service '{0}' already had a deployment running and message was not allowed to upgrade. Skipped deployment.", message.HostedServiceName);
+                    return;
+                }
+
+                
+                XDocument document = null;
+                if (!string.IsNullOrWhiteSpace(message.PackageConfigurationUri))
+                    document = XDocument.Load(message.PackageConfigurationUri);
+                else if (message.PackageConfiguration != null)
+                    document = XDocument.Parse(message.PackageConfiguration);
+
+                if (document == null)
+                    throw new Exception("The message did not have a valid PackageConfiguration Setting, specify either uri or xml");
+
+                await HandleServiceCertificates(message, management, document);
+
+
+                var result = await management.Deployments.CreateAsync(message.HostedServiceName,
+                       DeploymentSlot.Production,
+                       new DeploymentCreateParameters
+                       {
+                           StartDeployment = true,
+                           PackageUri = new Uri(message.HostedServicePackageSasUri),
+                           Name = message.DeploymentName,
+                           Label = message.DeploymentLabel ?? message.DeploymentName,
+                           Configuration = document.ToString(),
+                           ExtendedProperties = JsonConvert.DeserializeObject<Dictionary<string, string>>(message.DeploymentJsonExtendedProperties)
+                        
+                       });
+
+                bool running = false;
+                var starttime = DateTime.UtcNow;
+                while (!running)
+                {
+                    var production = await management.Deployments.GetBySlotAsync(message.HostedServiceName, DeploymentSlot.Production);
+                    if (production.RoleInstances.Any())
+                    {
+                        running = production.RoleInstances.All(instance => instance.InstanceStatus == "ReadyRole");
+                    }
+                    var waittime = (DateTime.UtcNow - starttime);
+
+                    if(waittime.TotalMinutes > message.DeploymentTimeoutInMinutes )
+                    {
+                        throw new Exception("Deployment Timeout: " + string.Join(", ",production.RoleInstances.Select(i =>i.InstanceStatus)));
+                    }
+                    if (!running)
+                        await Task.Delay(5000);
+                }
+
+            }
+        }
+
+        private async Task HandleServiceCertificates(DeployAzureHostedServiceMessage message, Microsoft.WindowsAzure.Management.Compute.ComputeManagementClient management, XDocument document)
+        {
+            var certificatesNeeded = document.Descendants()
+                .Where(e => e.Name.LocalName.Equals("Certificate"))
+                .Select(e => (string)e.Attribute("thumbprint")).ToArray();
+
+            if (!certificatesNeeded.Any())
+                return;
+
+            var existingCertificates = await management.ServiceCertificates.ListAsync(message.HostedServiceName);
+
+            foreach (var certificateThumbprint in certificatesNeeded)
+            {
+
+                if (existingCertificates.Certificates.Any(
+                        c => string.Equals(c.Thumbprint, certificateThumbprint, StringComparison.OrdinalIgnoreCase)))
+                {
+                    continue;
+                }
+
+                ServiceCertificateCreateParameters certificate = await certificates.GetServiceCertificateAsync(certificateThumbprint);
+
+
+                await management.ServiceCertificates.CreateAsync(message.HostedServiceName, certificate);
+
+            }
+        }
+
+        private static async Task CreateHostedService(DeployAzureHostedServiceMessage message, Microsoft.WindowsAzure.Management.Compute.ComputeManagementClient management)
+        {
+            if (!message.CanCreate)
+            {
+                throw new Exception("Service not created, message.CanCreate was false.");
+            }
+            var avalible = await management.HostedServices.CheckNameAvailabilityAsync(message.HostedServiceName);
+            if (!avalible.IsAvailable)
+            {
+                string exceptionMessage = "Service cloud not be created: " + avalible.Reason;
+
+                throw new Exception(exceptionMessage);
+            }
+
+
+            var operation = await management.HostedServices.CreateAsync(new HostedServiceCreateParameters
+            {
+                AffinityGroup = message.AffinityGroup,
+                Description = "Automatic Created Cloud Service",
+                Label = message.HostedServiceName,
+                ServiceName = message.HostedServiceName,
+                ExtendedProperties = JsonConvert.DeserializeObject<Dictionary<string, string>>(message.ServiceJsonExtendedProperties)
+            });
+        }
+    }
+}
---
+++ AzureWebRole.MessageProcessor.AzureHandlers/Messages/DeployAzureHostedServiceMessage.cs
@@ -0,0 +1,41 @@
+﻿using AzureWebRole.MessageProcessor.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureWebRole.MessageProcessor.AzureHandlers.Messages
+{
+    public class DeployAzureHostedServiceMessage : BaseMessage
+    {
+        public DeployAzureHostedServiceMessage()
+        {
+            CanCreate = false;
+            CanUpgrade = false;
+            DeploymentTimeoutInMinutes = 30;
+        }
+
+        public string AzureSubscriptionId { get; set; }
+        public string AzureSubscriptionCertificateThumbprint { get; set; }
+        public string AzureSubscriptionToken { get; set; }
+
+        public string HostedServicePackageSasUri{ get; set; }
+        public string PackageConfigurationUri { get; set; }
+        public string PackageConfiguration { get; set; }
+        public string HostedServiceName { get; set; }
+        public string AffinityGroup { get; set; }
+
+        public string ServiceJsonExtendedProperties { get; set; }
+        public string DeploymentJsonExtendedProperties { get; set; }
+
+        public bool CanCreate { get; set; }
+  
+        public bool CanUpgrade { get; set; }
+
+        public string DeploymentName { get; set; }
+        public string DeploymentLabel { get; set; }
+
+        public int DeploymentTimeoutInMinutes { get; set; }
+    }
+}
