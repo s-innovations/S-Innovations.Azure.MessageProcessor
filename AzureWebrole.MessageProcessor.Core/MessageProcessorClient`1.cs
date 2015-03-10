f3f0a3bd1637dbeac53c6a092aaefb120d16607c
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-03-10 19:36:21 +0100

    Add trace for every renew

--- AzureWebrole.MessageProcessor.Core/MessageProcessorClient`1.cs
+++ AzureWebrole.MessageProcessor.Core/MessageProcessorClient`1.cs
@@ -172,10 +172,11 @@ namespace SInnovations.Azure.MessageProcessor.Core
                     var MaximumTimeTask = Task.Delay(timeout);
                     while (loop)
                     {
+                        
                         var t = await Task.WhenAny(task, Task.Delay(_options.AutoRenewLockTimerDuration ?? DefaultLockRenewTimer), MaximumTimeTask);
                         if (t == MaximumTimeTask)
                             throw new TimeoutException(string.Format("The handler could not finish in given time :{0}", timeout));
-
+                        Trace.TraceInformation("Renewing Task<processingTask:{0}>", task.Status.ToString());
                         try
                         {
                             await _options.Provider.RenewLockAsync(message);
