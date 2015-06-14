92bb43a7909959655ab366b12af60ae58a5dc14a
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-06-15 01:41:13 +0200

    Added a quick attribute based way to dump information to ApplicationInsight

---
+++ MessageProcessor.ApplicationInsight/Attributes/ApplicationInsightsAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.Azure.MessageProcessor.ApplicationInsights.Attributes
+{
+    public class ApplicationInsightsAttribute : Attribute
+    {
+        public ApplicationInsightsAttribute()
+        {
+
+        }
+    }
+}
---
+++ MessageProcessor.ApplicationInsight/MessageCompletedNotificationExtensions.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.ApplicationInsights.DataContracts;
+using SInnovations.Azure.MessageProcessor.ApplicationInsights.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.Azure.MessageProcessor.Core.Notifications
+{
+    public static class MessageCompletedNotificationExtensions
+    {
+
+        public static EventTelemetry CreateEventTelemetry(this MessageCompletedNotification notice, string name = "MessageCompleted")
+        {
+            var t = new EventTelemetry(name)
+            {
+                Timestamp = DateTimeOffset.Now,
+            };
+            t.Properties.Add("MessageId", notice.Message.MessageId);
+            t.Properties.Add("MessageType", notice.Message.GetType().Name);
+            t.Metrics.Add("Elapsed", notice.Elapsed.TotalMilliseconds);
+
+            var props = notice.Message.GetType().GetProperties().Where(
+                prop => Attribute.IsDefined(prop, typeof(ApplicationInsightsAttribute)));
+
+            foreach (var prop in props)
+            {
+                t.Properties.Add(prop.Name, prop.GetValue(notice.Message).ToString());
+            }
+
+            return t;
+        }
+    }
+}
